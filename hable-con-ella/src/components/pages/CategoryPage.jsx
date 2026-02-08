e081788083ae3ce61c7c1e356bc94aca17180862
Dominic Jonas <ppkk.dominic@gmail.com>
2026-02-08 00:40:23 +0000

    toasters

--- hable-con-ella/src/components/pages/CategoryPage.jsx
+++ hable-con-ella/src/components/pages/CategoryPage.jsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import CategoryCard from "../common/CategoryCard";
 import Header from "../common/Header";
 import "./CategoryPage.scss";
+import { useAuth } from "../../hooks/useAuth";
 import { categories } from "../../data/categories";
 
 const CategoryPage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
   return (
     <>
-      <Header showBackButton={true} showRightContent={true} title="Categorías" />
+      <Header showBackButton={!user} showRightContent={true} title="Categorías" />
       <div className="category-page-container">
         <div className="category-card-container">
           {categories.map((category) => (
